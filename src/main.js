44dde58bfc194b17874aad421d0e542401f4c1bd
1ilir0lika <ilir8503@gmail.com>
2025-08-07 10:45:42 +0200

    toggle theme

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
-setupThemeToggle();
 document.addEventListener('DOMContentLoaded', () => {
 
   const encyclopedia = document.getElementById("encyclopedia");
   const addMainBtn = document.getElementById("add-main");
   const saveBtn = document.getElementById("save");
   const loadBtn = document.getElementById("load");
-  //const toggleBtn = document.getElementById('toggle-theme');
+  const toggleBtn = document.getElementById('toggle-theme');
+  // Applica il tema salvato
+  if (localStorage.getItem('tema') === 'scuro') {
+    document.body.classList.add('dark-mode');
+  }
   // Cambia tema al click
-  //toggleBtn.addEventListener('click', () => {
-  //  document.body.classList.toggle('dark-mode');
-  //  const isDark = document.body.classList.contains('dark-mode');
-  //  localStorage.setItem('tema', isDark ? 'scuro' : 'chiaro');
-  //});
+  toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    const isDark = document.body.classList.contains('dark-mode');
+    localStorage.setItem('tema', isDark ? 'scuro' : 'chiaro');
+  });
   // Utilità: crea elemento HTML
   function createElement(tag, className, textContent = '') {
     const el = document.createElement(tag);
