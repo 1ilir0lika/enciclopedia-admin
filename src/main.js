ccf6f62815f6ce3ecbedce0c8ddcda7b3516d4dd
1ilir0lika <ilir8503@gmail.com>
2025-08-07 10:06:47 +0200

    fix bug

--- src/main.js
+++ src/main.js
@@ -6,13 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const saveBtn = document.getElementById("save");
   const loadBtn = document.getElementById("load");
   const toggleBtn = document.getElementById('toggle-theme');
-  if (!toggleBtn) return;
-
   // Applica il tema salvato
   if (localStorage.getItem('tema') === 'scuro') {
     document.body.classList.add('dark-mode');
   }
-
   // Cambia tema al click
   toggleBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
